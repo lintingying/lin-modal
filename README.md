d94e3008e9f89d6530352601eb0c847ecb7a51ef
lintingying <277475478@qq.com>
2021-09-21 14:44:11 +0800

    css 压缩

--- README.md
+++ README.md
@@ -4,19 +4,34 @@
 npm install lin-modal
 ```
 
-In browser:
-
 ```html
 <link rel="stylesheet" type="text/css" href="modal.css">
 <script src="modal.js"></script>
 ```
 
 ```js
-var modal = new Modal();
+import Modal from 'lin-modal';
+import '../node_modules/lin-modal/dist/modal.css';
+
+const modal = new Modal();
 modal.open({
   title: '标题',
   content: '是否保存更改？',
+  other: '其他',
+  loadingText: '正在处理中...',
+  footer: [
+    {
+      text: '确定',
+      className: 'button primary-btn',
+      callback: modal.close();
+    },
+    {
+      text: '取消',
+      className: 'button',
+      callback: modal.close();
+    }
+  ],
 });
+
 modal.loading();
-modal.close();
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   mode: 'production',
   entry:  './src/modal.js', //library 设置与 entry 配置项绑定。对于绝大多数的库，指定一个入口就已经足够了
   plugins: [
-    new HtmlWebpackPlugin({ // 创建了一个全新的文件，所有的 bundle 会自动添加到 html 中。
-      title: 'modal'
-    }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin({
+      filename: "modal.css"
+    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"], // 推荐 production 环境的构建将 CSS 从你的 bundle 中分离出来，这样可以使用 CSS/JS 文件的并行加载。 这可以通过使用 mini-css-extract-plugin 来实现，因为它可以创建单独的 CSS 文件。 对于 development 模式（包括 webpack-dev-server），你可以使用 style-loader，因为它可以使用多个 标签将 CSS 插入到 DOM 中，并且反应会更快。
       },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      `...`,
+      new CssMinimizerPlugin(),
     ],
   },
   output: {
